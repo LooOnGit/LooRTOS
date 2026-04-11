db84d8a25a863e67e41525a82835117f82be42de
LooOnGit <tranconghoa24@gmail.com>
2026-04-11 20:24:27 +0700

    Update implement task

--- kernel/core/task.cpp
+++ kernel/core/task.cpp
@@ -1,22 +1,9 @@
 #include "task.hpp"
 
 TaskBase::TaskBase(TaskFunction entry, uint8_t priority)
-    : priority_(priority),
-      stackPtr_(nullptr),
-      next_(nullptr),
-      entry_(entry),
-      currentState_(State::READY) 
-{
+    : priority_(priority), stackPtr_(nullptr), next_(nullptr), entry_(entry),
+      currentState_(State::READY) {}
 
-}
+TaskBase::~TaskBase() {}
 
-TaskBase::~TaskBase() 
-{
-
-}
-
-TaskBase::State TaskBase::getState() const 
-{
-  return currentState_;
-}
-
+TaskBase::State TaskBase::getState() const { return currentState_; }
